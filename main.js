5a4d84301f400b05c72384d2d6f6e273af3a7b44
Developer Jahed <developerjahed@gmail.com>
2022-01-17 15:42:17 +0600

    Browser Alert And popupt

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("ok");
+// alert("hey")
+
+const coming=()=>{
+    alert("Coming");
+}
+const askPicnic=()=>{
+ const response=   confirm('are yoi going to picnic');
+ console.log(response);
+ if(response===true){
+     alert("amake free ta bkash kore de");
+ }else{
+     alert("Dure Giye Mor");
+ }
+}
+console.log("Ok");
